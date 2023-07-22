befba2253f67e322f7d89c148a2fed1e5d84b604
h3nr <henrique.s.oliveira@hotmail.com>
2023-07-22 00:47:05 -0300

    Salvamento Local

---
+++ scripts.js
@@ -0,0 +1,20 @@
+
+/*function typeWriter(elemento) {
+    const textoArray = elemento.innerHTML.split('');
+    elemento.innerHTML = '';
+    function atrasoInicial() {
+        textoArray.forEach((letra, i) => {
+            setTimeout(() => elemento.innerHTML += letra, 130 * i);
+        });
+    }
+    const tempoDeEspera = 1700;
+    setTimeout(atrasoInicial, tempoDeEspera);
+}
+
+const palavras = document.querySelector('#bio-name');
+typeWriter(palavras);*/
+
+
+
+
+
